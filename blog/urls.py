7c68c4e87a98b3fb2c7dc1f7976bfa754f03e8de
leitoBY <freeleito@mail.ru>
2016-12-25 11:21:32 +0300

    url
    some urls config

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list')
+    url(r'^post_list/', views.post_list, name='post_list'),
+    url(r'^$', views.blog, name='blog'),
 ]
